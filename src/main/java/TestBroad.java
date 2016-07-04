e32fc37ee64af07b03f94179a6fc50f62d69a72d
cynricshu <cynricshu@gmail.com>
2016-07-04 15:37:01 +0800

    测试了广播变量

--- src/main/java/TestBroad.java
+++ src/main/java/TestBroad.java
@@ -1,8 +1,11 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -14,24 +17,35 @@ public class TestBroad {
 
         SparkConf conf = new SparkConf().setAppName("Simple Application");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> inputData = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            inputData.add(i);
+        }
+        JavaRDD<Integer> numbers = sc.parallelize(inputData);
         Broadcast<Queue<Integer>> bc = sc.broadcast(new LinkedList<Integer>());
 
-        for (int i = 0; i < 5; i++) {
-            bc.getValue().add(i);
-        }
         Util.log("Size before iteration", bc.getValue().size());
 
-        while (true) {
-            if (bc.getValue().isEmpty()) {
-                break;
-            }
-            bc.getValue().poll();
-            bc.getValue().poll();
-//            bc.getValue().add(1);
-            Util.log("Size in iteration", bc.getValue().size());
-        }
+//        while (true) {
+//            if (bc.getValue().isEmpty()) {
+//                break;
+//            }
+//            bc.getValue().poll();
+//            bc.getValue().poll();
+////            bc.getValue().add(1);
+//            Util.log("Size in iteration", bc.getValue().size());
+//        }
 
-        Util.log(bc.getValue().size());
+        // 可以同时增减
+        numbers.foreach(i -> {
+            if (i % 2 == 0) {
+                bc.getValue().poll();
+                bc.getValue().add(i);
+            }
+        });
+
+        Util.log("Size after iteration", bc.getValue().size());
 
 
     }
